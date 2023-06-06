3a8b8f7eb0c32b585d0d0d200f5c14b1931ed02a
Nicole Planeta <60528262+Eternalon03@users.noreply.github.com>
2023-06-06 12:02:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,106 @@ from io import StringIO
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+import joblib
+import re
+import string
+
+import numpy as np
+import pandas as pd
+
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score, cohen_kappa_score, f1_score, classification_report
+from sklearn.model_selection import StratifiedKFold, train_test_split
+from sklearn.naive_bayes import MultinomialNB
+
+
+def categoryChecker(data, df):
+
+    # print("here")
+    # categories = [
+    #     'alt.atheism',
+    #     'comp.graphics',
+    #     'comp.os.ms-windows.misc',
+    #     'comp.sys.ibm.pc.hardware',
+    #     'comp.sys.mac.hardware',
+    #     'comp.windows.x',
+    #     'misc.forsale',
+    #     'rec.autos',
+    #     'rec.motorcycles',
+    #     'rec.sport.baseball',
+    #     'rec.sport.hockey',
+    #     'sci.crypt',
+    #     'sci.electronics',
+    #     'sci.med',
+    #     'sci.space',
+    #     'soc.religion.christian',
+    #     'talk.politics.guns',
+    #     'talk.politics.mideast',
+    #     'talk.politics.misc',
+    #     'talk.religion.misc'
+    # ]
+
+    # news_group_data = fetch_20newsgroups(
+    #     subset="all", remove=("headers", "footers", "quotes"), categories=categories
+    # )
+
+    # newspaperDataDF = pd.DataFrame(
+    #     dict(
+    #         text=news_group_data["data"],
+    #         target=news_group_data["target"]
+    #     )
+    # )
+    # newspaperDataDF["target"] = newspaperDataDF.target.map(lambda x: categories[x])
+
+    # newspaperDataDF["clean_text"] = newspaperDataDF.text.map(process_text)
+
+    # df_train, df_test = train_test_split(newspaperDataDF, test_size=0.20, stratify=newspaperDataDF.target)
+
+    # vec = CountVectorizer(
+    # ngram_range=(1, 3), 
+    # stop_words="english",
+    # )
+
+    # X_train = vec.fit_transform(df_train.clean_text)
+    # X_test = vec.transform(df_test.clean_text)
+
+    # y_train = df_train.target
+    # y_test = df_test.target
+
+    # nb = MultinomialNB()
+    # nb.fit(X_train, y_train)
+
+    # preds = nb.predict(X_test)
+
+    # joblib.dump(nb, "nb.joblib")
+    # joblib.dump(vec, "vec.joblib")
+
+    nb_saved = joblib.load("nb.joblib")
+    vec_saved = joblib.load("vec.joblib")
+
+    columns = customJSONArrayParser(data['columns'])
+
+    for column in columns:
+        nameOfNewColumn = 'Prediction of Column " ' + str(column) + '"'
+        for index, row in df.iterrows():
+            sample_text = [str(row[column])]
+            # Process the text in the same way you did when you trained it!
+            clean_sample_text = process_text(sample_text)
+            sample_vec = vec_saved.transform(sample_text)
+            print(nb_saved.predict(sample_vec))
+            df.loc[df.index[index], nameOfNewColumn] = nb_saved.predict(sample_vec)
+
+
+
+def process_text(text):
+    text = str(text).lower()
+    text = re.sub(
+        f"[{re.escape(string.punctuation)}]", " ", text
+    )
+    text = " ".join(text.split())
+    return text
+
 def customJSONArrayParser(arrayString):
     finalArray = [] 
     while('{"value":"' in arrayString):
@@ -32,6 +132,7 @@ def filter(data,df):
                             removeRow = False
                     elif(data['boolFilterOut'] == True and data['boolMatchExact'] == False):
                         if(keyword in str(row[column])):
+                            print("here")
                             removeRow = False
                     elif(data['boolFilterOut'] == False and data['boolMatchExact'] == True):
                         if(str(row[column]) == keyword):
@@ -49,9 +150,6 @@ def split(data,df):
     splitter = data['splitters']
     splitcheck1 = splitter.find('[/column]')
     splitcheck2 = splitter.find('[/cell-value]')
-
-    print(splitcheck1)
-    print(splitcheck2)
 
     if(splitcheck1 < splitcheck2):
         first = splitter[0:splitter.find('[/column]')]
@@ -78,14 +176,12 @@ def split(data,df):
                             break
                         start = string.find(second)
                         columnmake = string[0:start]
-                        print(columnmake)
                         string = string[start+len(second) :: ]
                     if(third != "" and string.find(third) != -1):
                         start = string.find(second)
                         tempstring = string[0:start]
                         end = tempstring.rfind(third)
                         cell = string[0:end]
-                        print(cell)
                         if(splitcheck1 < splitcheck2):
                             df.loc[index, columnmake] = str(cell)
                         else:
@@ -101,7 +197,6 @@ def split(data,df):
                             break
                         start = string.find(first)
                         cell = string[0:start]
-                        print(cell)
                         if(splitcheck1 < splitcheck2):
                             df.loc[index, columnmake] = str(cell)
                         elif(splitcheck1 > splitcheck2):
@@ -137,7 +232,6 @@ def merge(data,df):
             df.drop([column], axis=1, inplace=True)
 
                     
-
 #Set up Flask
 app = Flask(__name__)
 cors = CORS(app)
@@ -154,10 +248,9 @@ def postME():
         split(data,df)
     if(data['function'] == "Merge Columns by Characters"):
         merge(data,df)
-        
-    print(data)
+    if(data['function'] == "Predict Column Category"):
+        categoryChecker(data,df)
 
-    print(df)
         
     # end
     arrayofarrays = []
